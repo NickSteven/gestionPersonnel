5c31b7414d51df94b0d9c77af2a0137bff14dd93
unknown <nickandoniaina@gmail.com>
2021-03-31 16:28:13 +0100

    controller v2

--- src/Controller/PersonnelController.php
+++ src/Controller/PersonnelController.php
@@ -6,8 +6,13 @@ namespace App\Controller;
 use App\Entity\Employe;
 use App\Repository\EmployeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\EmployeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class PersonnelController extends AbstractController
 {	
@@ -79,5 +84,64 @@ class PersonnelController extends AbstractController
     	return $this->render('personnel/gest_permission.html.twig', [
     		'permission' => 'permissions'
     	]);
+    }
+
+    // Création d'un nouveau employé
+    /**
+     * @Route("/gest_personnel/new", name="new_employe")
+     * Method({"GET", "POST"})
+     */
+    public function new(Request $request) {
+    	$employe = new Employe();
+
+    	$form = $this->createFormBuilder($employe)
+    				 ->add('nom', TextType::class, array(
+    				 	'required' => true,
+    				 	'attr' => array('class' => 'form-control')
+    				 ))
+    				 ->add('prenom', TextType::class, array(
+    				 	'required' => true,
+    				 	'attr' => array('class' => 'form-control')
+    				 ))
+    				 ->add('adresse', TextType::class, array(
+    				 	'required' => true,
+    				 	'attr' => array('class' => 'form-control')
+    				 ))
+    				 ->add('fonction', TextType::class, array(
+    				 	'required' => true,
+    				 	'attr' => array('class' => 'form-control')
+    				 ))
+    				 ->add('save', SubmitType::class, array(
+    				 	'label' => 'Ajouter',
+    				 	'attr' => array('class' => 'btn btn-primary margin-top-3')
+    				 ))
+    				 ->getForm();
+
+    	$form->handleRequest($request);
+
+    	if($form->isSubmitted() && $form->isValid()) {
+    		$employe = $form->getData();
+
+    		$entityManager = $this->getDoctrine()->getManager();
+    		$entityManager->persist($employe);
+    		$entityManager->flush();
+
+    		return $this->redirectToRoute('personnel_show');
+    	}
+
+    	return $this->render('personnel/nouveau.html.twig', array(
+    		'form' => $form->createView()
+    	));
+    }
+
+    // Edition des employés
+    /**
+     * @Route ("/gest_personnel/{id}", name="employe_show")
+    */
+    public function show($id) {
+    	$employe = $this->getDoctrine()->getRepository(Employe::class)->find($id);
+
+    	return $this->render('personnel/affich_perso.html.twig', 
+    		array('employe' => $employe));
     }
 }
